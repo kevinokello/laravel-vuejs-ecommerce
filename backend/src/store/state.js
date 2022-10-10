0410d9c0a2486dabca8c2f4902f3b660688e065b
kevinokello <okellokevin77@gmail.com>
2022-10-10 18:10:54 +0300

    First commit

---
+++ backend/src/store/state.js
@@ -0,0 +1,52 @@
+export default {
+  user: {
+    token: sessionStorage.getItem('TOKEN'),
+    data: {}
+  },
+  products: {
+    loading: false,
+    data: [],
+    links: [],
+    from: null,
+    to: null,
+    page: 1,
+    limit: null,
+    total: null
+  },
+  users: {
+    loading: false,
+    data: [],
+    links: [],
+    from: null,
+    to: null,
+    page: 1,
+    limit: null,
+    total: null
+  },
+  customers: {
+    loading: false,
+    data: [],
+    links: [],
+    from: null,
+    to: null,
+    page: 1,
+    limit: null,
+    total: null
+  },
+  countries: [],
+  orders: {
+    loading: false,
+    data: [],
+    links: [],
+    from: null,
+    to: null,
+    page: 1,
+    limit: null,
+    total: null
+  },
+  toast: {
+    show: false,
+    message: '',
+    delay: 5000
+  }
+}
